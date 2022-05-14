724df401789d509e20f7e062c7672ffb1cc96352
LightningFz <96966812+LightningFz@users.noreply.github.com>
2022-05-14 14:53:35 -0400

    movement

--- masseyhack/Assets/scripts/playerMovemnet.cs
+++ masseyhack/Assets/scripts/playerMovemnet.cs
@@ -4,9 +4,47 @@ using UnityEngine;
 
 public class playerMovemnet : MonoBehaviour
 {
-    // Update is called once per frame
+    public float runSpeed = 40;
+    public float jumpPower = 5;
+
+    private bool _isJumping = false;
+    private bool isGrounded;
+    private float horizontal;
+    private int jumpCount;
+
+    Rigidbody2D rb;
+    BoxCollider2D coll;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        coll = GetComponent<BoxCollider2D>();
+    }
     void Update()
     {
-        
+        getInput();
+    }
+    private void FixedUpdate()
+    {
+        move();
+    }
+
+    private void getInput()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        if (Input.GetButtonDown("Jump"))
+        {
+            _isJumping = true;
+        }
+
+    }
+    private void move()
+    {
+        rb.velocity = new Vector2(horizontal * runSpeed, rb.velocity.y);
+        if (_isJumping && jumpCount > 0)
+        {
+            rb.AddForce(new Vector2(0f, jumpPower));
+            jumpCount--;
+        }
+        _isJumping = false;
     }
 }
