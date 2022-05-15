4c58d71f7360a3c05fad761c094b8452d7dedc41
LightningFz <96966812+LightningFz@users.noreply.github.com>
2022-05-15 07:56:14 -0400

    healthbar

--- masseyhack/Assets/scripts/playerHealth.cs
+++ masseyhack/Assets/scripts/playerHealth.cs
@@ -7,16 +7,19 @@ public class playerHealth : MonoBehaviour
 {
     public int maxhealth;
     public HealthSystem healthSystem;
+    public healthbar healthbar;
     // Start is called before the first frame update
     void Start()
     {
         healthSystem = new HealthSystem(maxhealth);
+        healthbar.SetMaxHealth(healthSystem.healthMax);
     }
 
     // Update is called once per frame
     void Update()
     {
         Die();
+        healthbar.SetHealth(healthSystem.health);
     }
     public void Die()
     {
